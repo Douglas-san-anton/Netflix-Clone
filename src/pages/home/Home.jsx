cb99f2771eeeb897df6532c8c845b41dd7d26d4d
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-01-31 16:23:24 -0300

    create form in hero

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
   return (
     <div className={Styles.container}>
       <div className={Styles.hero__bg}>
-        <div>
+
           <nav className={Styles.nav__container}>
             <span className={Styles.nav__span}><img src={logo} alt="logo" /></span>
             <div>
@@ -15,18 +15,33 @@ export const Home = () => {
                 <option value="/ar/" data-language="es">Español</option>
                 <option value="/ar-en/" data-language="en">English</option>
               </select>
+              <Link className={Styles.nav__button} to={'/login'}>Iniciar sesión</Link>
             </div>
-            <Link className={Styles.nav__button} to={'/login'}>Iniciar sesión</Link>
           </nav>
           <div className={Styles.hero__bg_opacity}></div>
-        </div>
+
+          <div className={Styles.hero__card}>
+            <h1>Películas y series ilimitadas y mucho más</h1>
+            <h2>Disfruta donde quieras. Cancela cuando quieras.</h2>
+
+            <form className={Styles.hero__form}>
+              <h3>¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o reiniciar tu membresía de Netflix.</h3>
+              <div className={Styles.hero__form_email}>
+
+                <label className={Styles.hero__form_input}>
+                  <input placeholder='Email' type="text" />
+                </label>
+
+                <button className={Styles.hero__form_btn}>
+                  <span>Comenzar</span>
+                  <span>{'>'}</span>
+                </button>
+
+              </div>
+            </form>
+          </div>
 
       </div>
-
-        {/* <div className={Styles.hero__card}>
-
-          <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel doloribus reprehenderit nostrum dolores magni quasi et adipisci natus assumenda, inventore saepe quis deleniti dolorum repudiandae nam tempora? Deleniti, praesentium unde.</div>
-        </div> */}
     </div>
  )
 }
