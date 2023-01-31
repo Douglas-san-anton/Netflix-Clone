d25052cc79f0f9c1f1599ea8da34c485b111eed6
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-01-31 09:02:32 -0300

    changes in hero section in home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
-import Styles from './App.module.css'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Home } from './pages/home/Home';
+import { Login } from './pages/login/Login';
 
 function App() {
 
   return (
-    <div className={Styles.container}>
-      
-    </div>
+      <Router>
+        <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/login' element={<Login />}/>
+        </Routes>
+      </Router>
   )
 }
 
-export default App
+export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Styles from './Home.module.css'
+import logo from '../../assets/netflix.png'
 
 export const Home = () => {
   return (
     <div className={Styles.container}>
-      <nav className={Styles.nav__container}>
-        <span></span>
-        <div></div>
-        <Link to={'/login'}>Iniciar sesión</Link>
-      </nav>
+      <div className={Styles.hero__bg}>
+        <div>
+          <nav className={Styles.nav__container}>
+            <span className={Styles.nav__span}><img src={logo} alt="logo" /></span>
+            <div>
+              <select className={Styles.nav__select}>
+                <option value="/ar/" data-language="es">Español</option>
+                <option value="/ar-en/" data-language="en">English</option>
+              </select>
+            </div>
+            <Link className={Styles.nav__button} to={'/login'}>Iniciar sesión</Link>
+          </nav>
+          <div className={Styles.hero__bg_opacity}></div>
+        </div>
+
+      </div>
+
+        {/* <div className={Styles.hero__card}>
+
+          <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel doloribus reprehenderit nostrum dolores magni quasi et adipisci natus assumenda, inventore saepe quis deleniti dolorum repudiandae nam tempora? Deleniti, praesentium unde.</div>
+        </div> */}
     </div>
  )
 }
