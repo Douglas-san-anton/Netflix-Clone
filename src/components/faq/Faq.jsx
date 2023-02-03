bb5e4ab01911c26adc7c0d99b9478df54be162d6
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-02-03 10:04:03 -0300

    modification FAQ details

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -10,11 +10,11 @@ export const Faq = () => {
           <li>
             <button className={Styles.faq__question}>
               {"¿Qué es Netflix?"}
-              <svg id="thin-x" viewBox="0 0 26 26" focusable="true" className={Styles.svg__closed}>
+              <svg id="thin-x" viewBox="0 0 26 26" focusable="true" className={Styles.svg__open} >
                 <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
               </svg>
             </button>
-            <div>
+            <div className={Styles.faq__answer}>
               <span>
                 Netflix es un servicio de streaming que ofrece una gran variedad de películas, series y documentales premiados en casi cualquier pantalla conectada a internet.
                 <br />
@@ -23,7 +23,7 @@ export const Faq = () => {
               </span>
             </div>
           </li>
-          
+
         </ul>
       </div>
     </section>
