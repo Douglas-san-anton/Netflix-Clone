c9057e3deada7360055f1b02a0916999b16af9d7
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-02-08 16:17:43 -0300

    background and button fixes on login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -8,67 +8,69 @@ import { FooterLogin } from '../../components/footer-login/FooterLogin'
 export const Login = () => {
   return (
     <div className={Styles.login__wrapper}>
-      <div className={Styles.login__wrapper_bg}>
-        <img className={Styles.login__wrapper_img} src={bg} alt="" />
-      </div>
-      <div className={Styles.login__header}>
-        <Link to={'/'} className={Styles.login__logo}><img src={logo} alt="" /></Link>
-      </div>
+      <div className={Styles.login__bg}>
+        <div className={Styles.login__wrapper_bg}>
+          <img className={Styles.login__wrapper_img} src={bg} alt="" />
+        </div>
+        <div className={Styles.login__header}>
+          <Link to={'/'} className={Styles.login__logo}><img src={logo} alt="" /></Link>
+        </div>
 
-      <div className={Styles.login__body}>
-        <div className={Styles.login__content}>
-          <div className={Styles.login__form_main}>
-            <h1>Inicia sesión</h1>
+        <div className={Styles.login__body}>
+          <div className={Styles.login__content}>
+            <div className={Styles.login__form_main}>
+              <h1>Inicia sesión</h1>
 
-            <form method='post'>
-              <div className={Styles.login__form_container}>
-                <label className={Styles.login__label_container}>
-                  <input type="text" />
-                  <label>Email o número de teléfono</label>
-                </label>
-              </div>
-
-              <div className={Styles.login__form_container}>
-                <label className={Styles.login__label_container}>
-                  <input type="password" />
-                  <label>Contraseña</label>
-                </label>
-              </div>
-
-
-              <button className={Styles.login__btn}>Iniciar sesión</button>
-              <div className={Styles.login__help}>
-                <div className={Styles.login__remember_me}>
-                  <input type="checkbox" />
-                  <label>
-                    <span className={Styles.login__remember_me_span}>Recuérdame</span>
+              <form method='post'>
+                <div className={Styles.login__form_container}>
+                  <label className={Styles.login__label_container}>
+                    <input type="text" />
+                    <label>Email o número de teléfono</label>
                   </label>
                 </div>
-                <a className={Styles.login__help_link} target="_self" href="/LoginHelp">¿Necesitas ayuda?</a>
+
+                <div className={Styles.login__form_container}>
+                  <label className={Styles.login__label_container}>
+                    <input type="password" />
+                    <label>Contraseña</label>
+                  </label>
+                </div>
+
+
+                <button className={Styles.login__btn}>Iniciar sesión</button>
+                <div className={Styles.login__help}>
+                  <div className={Styles.login__remember_me}>
+                    <input type="checkbox" />
+                    <label>
+                      <span className={Styles.login__remember_me_span}>Recuérdame</span>
+                    </label>
+                  </div>
+                  <a className={Styles.login__help_link} target="_self" href="/LoginHelp">¿Necesitas ayuda?</a>
+                </div>
+              </form>
+            </div>
+
+            <div className={Styles.login__form_other}>
+              <div className={Styles.login__signup_now}>
+              {'¿Primera vez en Netflix? '}
+              <a target="_self" href="/">Suscríbete ahora</a>
+              {'.'}
               </div>
-            </form>
-          </div>
-
-          <div className={Styles.login__form_other}>
-            <div className={Styles.login__signup_now}>
-            {'¿Primera vez en Netflix? '}
-            <a target="_self" href="/">Suscríbete ahora</a>
-            {'.'}
+              <div className={Styles.login__recaptcha}>
+                <p>
+                  <span>Esta página está protegida por Google reCAPTCHA para comprobar que no eres un robot.</span>
+                  &nbsp;
+                  <button className={Styles.login__recaptcha_btn}>Más info.</button>
+                </p>
+              </div>
+              <div className={Styles.login__recaptcha_span}></div>
             </div>
-            <div className={Styles.login__recaptcha}>
-              <p>
-                <span>Esta página está protegida por Google reCAPTCHA para comprobar que no eres un robot.</span>
-                &nbsp;
-                <button className={Styles.login__recaptcha_btn}>Más info.</button>
-              </p>
-            </div>
-            <div className={Styles.login__recaptcha_span}></div>
-          </div>
 
+          </div>
         </div>
-      </div>
 
-      <FooterLogin />
+        <FooterLogin />
+      </div>
     </div>
   )
 }
