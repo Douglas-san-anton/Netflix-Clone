5ed7d6e7e24715c9c9b7935b8345e0b517ef6339
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-02-17 17:11:02 -0300

    modification in poster card

--- src/pages/browse/Browse.jsx
+++ src/pages/browse/Browse.jsx
@@ -3,6 +3,7 @@ import Styles from './Browse.module.css'
 import { MainBrowse } from '../../components/main-browse/MainBrowse'
 import { NavBrowse } from '../../components/nav-browse/NavBrowse'
 import { CatalogueBrowse } from '../../components/catalogue-browse/CatalogueBrowse'
+import { Footer } from '../../components/footer/Footer.jsx'
 
 export const Browse = () => {
   return (
@@ -10,6 +11,7 @@ export const Browse = () => {
       <NavBrowse />
       <MainBrowse />
       <CatalogueBrowse />
+      <Footer />
     </div>
   )
 }
