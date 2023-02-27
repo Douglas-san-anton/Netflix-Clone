dda66378d3943e9bf5fce7aaf4e8975a37c9e4c1
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-02-27 11:55:39 -0300

    created styles in movie browse

--- src/components/catalogue-browse/CatalogueBrowse.jsx
+++ src/components/catalogue-browse/CatalogueBrowse.jsx
@@ -1,34 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 import Styles from '../catalogue-browse/CatalogueBrowse.module.css'
+import { MovieBrowse } from '../movie-browse/MovieBrowse'
 
 export const CatalogueBrowse = () => {
 
-  const API_URL = 'https://api.themoviedb.org/3'
-  const API_KEY = '6fb2ca4be7447bcb8f45a23d1ff94045'
-  const URL_IMAGE = 'https://image.tmdb.org/t/p/original'
-
-  const [movies, setMovies] = useState([])
-
-  const fetchMovies = async (cb) => {
-    const { data: { results },
-    } = await axios.get(`${API_URL}/movie/popular`, {
-      params: {
-        api_key: API_KEY
-      },
-    })
-
-    cb(results)
-    console.log(results);
-  }
-
-  useEffect(() => {
-    fetchMovies(setMovies)
-  }, [])
-
   return (
     <div className={Styles.container}>
-      {movies.map((movie) => <img key={movie.id} src={`${URL_IMAGE + movie.poster_path}`} />)}
+      <div className={Styles.slider}>
+        <h2 className={Styles.titles}>Populares en Netfilx</h2>
+        <MovieBrowse />
+      </div>
+      <div className={Styles.slider}>
+        <h2 className={Styles.titles}>Tendencias</h2>
+        <MovieBrowse />
+      </div>
+      <div className={Styles.slider}>
+        <h2 className={Styles.titles}>Series</h2>
+        <MovieBrowse />
+      </div>
+      <div className={Styles.slider}>
+        <h2 className={Styles.titles}>Documentales</h2>
+        <MovieBrowse />
+      </div>
     </div>
   )
 }
--- src/components/movie-browse/MovieBrowse.jsx
+++ src/components/movie-browse/MovieBrowse.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Styles from '../movie-browse/MovieBrowse.module.css'
+import axios from 'axios'
 
 export const MovieBrowse = () => {
+
+  const API_URL = 'https://api.themoviedb.org/3'
+  const API_KEY = '6fb2ca4be7447bcb8f45a23d1ff94045'
+  const URL_IMAGE = 'https://image.tmdb.org/t/p/original'
+
+  const [movies, setMovies] = useState([])
+
+  const fetchMovies = async (cb) => {
+    const { data: { results },
+    } = await axios.get(`${API_URL}/movie/popular`, {
+      params: {
+        api_key: API_KEY
+      },
+    })
+
+    cb(results)
+    console.log(results);
+  }
+
+  useEffect(() => {
+    fetchMovies(setMovies)
+  }, [])
+
   return (
-    <div>MovieBrowse</div>
+    <div className={Styles.container}>
+      {/* {movies.map((movie) => <img key={movie.id} src={`${URL_IMAGE + movie.poster_path}`} />)} */}
+      {movies.map((movie) => <img className={Styles.img} key={movie.id} src={`${URL_IMAGE + movie.poster_path}`} />)}
+    </div>
   )
 }
