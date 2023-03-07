98f3b68768a494e371b48e0cd3083a136cd98927
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-03-07 19:37:14 -0300

    i18n folder creation plus language files

---
+++ src/i18n/index.js
@@ -0,0 +1,13 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslation from './en.json'
+import esTranslation from './es.json'
+
+const resources = {
+  en: {
+    translation: enTranslation,
+  },
+  es: {
+    translation: esTranslation,
+  }
+}
