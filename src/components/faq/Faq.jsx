22d78e6bc3d98d6b5c5fed7d50b0abbafeb6deba
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2023-03-08 23:42:38 -0300

    dictionary expansion in faq finished

--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -20,76 +20,76 @@ export const Faq = () => {
               </svg>
             </label>
             <div className={Styles.faq__answer}>
-              Netflix es un servicio de streaming que ofrece una gran variedad de películas, series y documentales premiados en casi cualquier pantalla conectada a internet.
+              {t('home.faq.what-is-netflix-answer1')}
               <br />
               <br />
-              Todo lo que quieras ver, sin límites ni comerciales, a un costo muy accesible. Siempre hay algo nuevo por descubrir, ¡y todas las semanas se agregan más películas y series!
+              {t('home.faq.what-is-netflix-answer1')}
             </div>
           </li>
           <li className={Styles.faq__questions}>
             <input type="checkbox" id="q2" />
             <label className={Styles.faq__question} for="q2">
-              {"¿Cuánto cuesta Netflix?"}
+              {t('home.faq.how-much.title')}
               <svg id="thin-x" viewBox="0 0 26 26" focusable="true" className={Styles.svg__open} >
                 <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
               </svg>
             </label>
             <div className={Styles.faq__answer}>
-              Disfruta Netflix en tu smartphone, tablet, smart TV, laptop o dispositivo de streaming, todo por una tarifa plana mensual. Planes desde $ 699 hasta $ 1.899 al mes (sin impuestos incluidos). Sin costos adicionales ni contratos.
+              {t('home.faq.how-much.answer')}
             </div>
           </li>
           <li className={Styles.faq__questions}>
             <input type="checkbox" id="q3" />
             <label className={Styles.faq__question} for="q3">
-              {"¿Dónde puedo ver Netflix?"}
+              {t('home.faq.where.title')}
               <svg id="thin-x" viewBox="0 0 26 26" focusable="true" className={Styles.svg__open} >
                 <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
               </svg>
             </label>
             <div className={Styles.faq__answer}>
-              Disfruta donde quieras, cuando quieras. Inicia sesión en tu cuenta de Netflix para ver contenido al instante a través de netflix.com desde tu computadora personal o en cualquier dispositivo con conexión a internet que cuente con la app de Netflix, como smart TV, smartphones, tablets, reproductores multimedia y consolas de juegos.
+              {t('home.faq.where.answer1')}
               <br />
               <br />
-              Además, puedes descargar tus series favoritas con iOS, Android o la app para Windows 10. Con la función de descarga, puedes ver donde vayas y sin conexión a internet. Lleva Netflix contigo adonde sea.
+              {t('home.faq.where.answer2')}
             </div>
           </li>
           <li className={Styles.faq__questions}>
             <input type="checkbox" id="q4" />
             <label className={Styles.faq__question} for="q4">
-              {"¿Cómo cancelo?"}
+              {t('home.faq.how-do.title')}
               <svg id="thin-x" viewBox="0 0 26 26" focusable="true" className={Styles.svg__open} >
                 <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
               </svg>
             </label>
             <div className={Styles.faq__answer}>
-              Netflix es flexible. Sin contratos molestos ni compromisos. Cancela la membresía online con solo dos clics. No hay cargos por cancelación. Empieza y termina cuando quieras.
+              {t('home.faq.how-do.answer')}
             </div>
           </li>
           <li className={Styles.faq__questions}>
             <input type="checkbox" id="q5" />
             <label className={Styles.faq__question} for="q5">
-              {"¿Qué puedo ver en Netflix?"}
+              {t('home.faq.what-can-i.title')}
               <svg id="thin-x" viewBox="0 0 26 26" focusable="true" className={Styles.svg__open} >
                 <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
               </svg>
             </label>
             <div className={Styles.faq__answer}>
-              Netflix tiene un amplio catálogo de películas, series, documentales, animes, originales premiados y más. Todo lo que quieras ver, cuando quieras.
+              {t('home.faq.what-can-i.answer')}
             </div>
           </li>
           <li className={Styles.faq__questions}>
             <input type="checkbox" id="q6" />
             <label className={Styles.faq__question} for="q6">
-              {"¿Es bueno Netflix para los niños?"}
+              {t('home.faq.is-netflix-good.title')}
               <svg id="thin-x" viewBox="0 0 26 26" focusable="true" className={Styles.svg__open} >
                 <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
               </svg>
             </label>
             <div className={Styles.faq__answer}>
-              La experiencia de Netflix para niños está incluida en la membresía para que los padres tengan el control mientras los peques disfrutan series y películas familiares en su propio espacio.
+              {t('home.faq.is-netflix-good.answer1')}
               <br />
               <br />
-              Los perfiles para niños incluyen controles parentales protegidos por PIN que te permiten restringir el contenido que pueden ver los niños en función de la clasificación por edad y bloquear determinados títulos que no quieras que los niños vean.
+              {t('home.faq.is-netflix-good.answer2')}
             </div>
           </li>
 
